3611504dc7dd949b57bb83c36701c53e432ac345
DaveTech <haryourthundey1@gmail.com>
2024-07-03 18:08:21 +0100

    hng-task-1

---
+++ script.js
@@ -0,0 +1,12 @@
+function updateTimeAndDay() {
+    const now = new Date();
+    const isoString = now.toISOString();
+    const utcTime = isoString.slice(11, 19);
+    const dayOfWeek = now.toLocaleDateString("en-US", { weekday: "long" });
+  
+    document.getElementById("currentTimeUTC").textContent = utcTime;
+    document.getElementById("currentDay").textContent = dayOfWeek;
+  }
+  
+  updateTimeAndDay();
+  setInterval(updateTimeAndDay, 1000);
